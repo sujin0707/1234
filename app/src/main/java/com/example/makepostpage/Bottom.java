15b7c521390beec6ebf8099ede3c484ea000647a
HeyIn0513 <101650255+HeyIn0513@users.noreply.github.com>
2022-11-18 20:32:24 +0900

    MakePost_Test1

---
+++ app/src/main/java/com/example/makepostpage/Bottom.java
@@ -0,0 +1,14 @@
+package com.example.makepostpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bottom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom);
+    }
+}
